9d75861f534d1987e421aa2a7be76ee635ee9629
Thomas Chafe <chaft6751@gmail.com>
2025-11-16 17:47:56 -0500

    he

---
+++ Roulette.py
@@ -0,0 +1,21 @@
+from random import*
+from tkinter import*
+
+#setup
+root = Tk()
+
+#varable
+Board = PhotoImage()
+
+
+#methods
+
+#spin outcome
+outcome = randint(0,36)
+
+#get bet
+def get_bet():
+    input('options for bet:  ')
+#check outcome vs bet
+
+#class
